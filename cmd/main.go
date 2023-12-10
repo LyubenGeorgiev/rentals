e564f03c54b29c81a408cf78607722be93f6c078
LyubenGeorgiev <koko6kata1@gmail.com>
2023-12-10 13:32:18 +0200

    Simple db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5434
+	user     = "root"
+	password = "root"
+	dbname   = "testingwithrentals"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// Database connection
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	database, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer database.Close()
+
+	err = database.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
